dfbb1b2eb3b53f27185de9620fba3278129a08ef
Your Name <jonathan.r.gilbert@gmail.com>
2018-02-20 13:02:53 -0800

    round 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -58,55 +58,54 @@ function list() {
     }
 }
 
-$('body').on('click', '.choice', function () {
+$(document).on('click', '.choice', function () {
 
-    $(".choice").on("click", function () {
-        $("#3").empty();
+    $("#3").empty();
 
-        var game = $(this).attr("type");
+    var game = $(this).attr("type");
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            game + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        game + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
 
-                var results = response.data;
-                for (var i = 0; i < results.length; i++) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
 
-                    var gameDiv = $("<div>");
-                    var p = $("<p>").text("Rating: " + results[i].rating);
-                    var gameImage = $("<img>");
+                var gameDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                var gameImage = $("<img>");
 
-                    gameDiv.attr("class", "chain");
-                    p.attr("class", "chain");
-                    gameImage.attr("src", results[i].images.fixed_height_still.url);
-                    gameImage.attr("data-still", results[i].images.fixed_height_still.url);
-                    gameImage.attr("data-animate", results[i].images.fixed_height.url);
-                    gameImage.attr("data-state", "still");
-                    gameDiv.append(p);
-                    gameDiv.append(gameImage);
-                    $("#3").append(gameDiv);
-                }
-                $(".chain").on("click", function () {
-                    var state = $(this).attr("data-state");
-                    var animateUrl = $(this).attr("data-animate");
-                    var stillUrl = $(this).attr("data-still");
-                    console.log(state);
-                    console.log(animateUrl);
-                    console.log(stillUrl);
-                    if (state === "still") {
-                        $(this).attr("src", animateUrl);
-                        $(this).attr("data-state", "animate");
-                    } else {
-                        $(this).attr("src", stillUrl);
-                        $(this).attr("data-state", "still");
-                    }
-                });
-            });
-    });
+                gameDiv.attr("class", "chain");
+                p.attr("class", "chain");
+                gameImage.attr("src", results[i].images.fixed_height_still.url);
+                gameImage.attr("data-still", results[i].images.fixed_height_still.url);
+                gameImage.attr("data-animate", results[i].images.fixed_height.url);
+                gameImage.attr("data-state", "still");
+                gameDiv.append(p);
+                gameDiv.append(gameImage);
+                $("#3").append(gameDiv);
+            }
+        });
 });
+
+$("#3").on('click', '.chain', function () {
+
+function ender() {
+    var state = $(this).attr("data-state");
+    console.log(state);
+    if (state === "still") {
+        var animateUrl = $(this).attr("data-animate");
+        $(this).attr("src", animateUrl);
+    } else {
+        var stillUrl = $(this).attr("data-still");
+        $(this).attr("src", stillUrl);
+    }
+    }
+});
+
 list();
